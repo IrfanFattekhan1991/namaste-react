3f5b84664d983116b47a72bbd15ba3034322ba60
IrfanFattekhan <irfan.fattekhan1991@gmail.com>
2023-09-04 22:25:49 +0530

    Revert "second commit"
    This reverts commit 9ca953699b628d3d3510feb4ce6ddee97d667e3e.

--- App.js
+++ App.js
@@ -1,70 +1,15 @@
-import React, { lazy, Suspense } from "react";
-import ReactDOM from "react-dom/client";
-import Header from "./src/components/Header";
-import Body from "./src/components/Body";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import About from "./src/components/About";
-import Contact from "./src/components/Contact";
-import Cart from "./src/components/Cart";
-import Error from "./src/components/Error";
-import RestaurantMenu from "./src/components/RestaurantMenu";
-import { Provider } from "react-redux";
-import appStore from "./src/utils/appStore";
-//import Grocery from "./src/components/Grocery";
-
-const Grocery = lazy(() => import("./src/components/Grocery"));
-
-const AppLayout = () => {
-  return (
-    <Provider store={appStore}>
-      <div className="app">
-        <Header />
-        <Outlet />
-      </div>
-    </Provider>
-  );
-};
-
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Body />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/grocery",
-        element: (
-          <Suspense fallback={<h1>Loading..........</h1>}>
-            <Grocery />
-          </Suspense>
-        ),
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/restaurants/:resId",
-        element: <RestaurantMenu />,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-    ],
-    errorElement: <Error />,
-  },
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
 ]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter} />);
+console.log(parent);
+
+const root = ReactDOM.unstable_createRoot(document.querySelector("#root"));
+
+root.render(parent);
